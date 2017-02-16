bd5a8b6b8c0a7800164d2a488b6a55ce0ca741bc
whigy <HWAG@tobii.com>
2017-02-16 14:24:50 +0100

    assign 3

--- assign3_main.py
+++ assign3_main.py
@@ -298,3 +298,131 @@ global_step = tf.Variable(0)  # count the number of steps taken.
 learning_rate = tf.train.exponential_decay(0.5, global_step, ...)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 """
+
+batch_size = 128
+node_size1 = 1024
+node_size2 = 512
+beta = 0.001
+dropout_rate = 0.8
+
+graph = tf.Graph()
+with graph.as_default():
+
+  # Input data. For the training data, we use a placeholder that will be fed
+  # at run time with a training minibatch.
+  tf_train_dataset = tf.placeholder(tf.float32,
+                                    shape=(batch_size, image_size * image_size))
+  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+  tf_valid_dataset = tf.constant(valid_dataset)
+  tf_test_dataset = tf.constant(test_dataset)
+  
+  global_step = tf.Variable(0)  # count the number of steps taken.
+  learning_rate = tf.train.exponential_decay(0.5, global_step, 1000, 0.96)
+  ##decayed_learning_rate = learning_rate * decay_rate ^ (global_step / decay_steps)
+  ##tf.train.exponential_decay(learning_rate, global_step, decay_steps, decay_rate, staircase=False, name=None)
+  
+  
+  # Variables.
+  weights1 = tf.get_variable("weights1", shape=[image_size * image_size, node_size1],
+           initializer=tf.contrib.layers.xavier_initializer())
+  #weights1 = tf.Variable(
+  #  tf.truncated_normal([image_size * image_size, node_size1]))
+  biases1 = tf.Variable(tf.zeros([node_size1]))
+
+  weights2 = tf.get_variable("weights2", shape=[node_size1, node_size2],
+           initializer=tf.contrib.layers.xavier_initializer())
+  biases2 = tf.Variable(tf.zeros([node_size2]))
+
+  weights3 = tf.get_variable("weights3", shape=[node_size2, num_labels],
+           initializer=tf.contrib.layers.xavier_initializer())
+  biases3 = tf.Variable(tf.zeros([num_labels]))
+  
+  
+  def forward_pass(data, dropout_rate):
+  # Training computation.
+      layer1 = tf.matmul(data, weights1) + biases1
+      relu1 = tf.nn.relu(layer1)
+      drop1 = tf.nn.dropout(relu1, dropout_rate)
+      layer2 = tf.matmul(drop1, weights2) + biases2
+      relu2 = tf.nn.relu(layer2)
+      drop2 = tf.nn.dropout(relu2, dropout_rate)
+      layer3 = tf.matmul(drop2, weights3) + biases3
+      return layer3
+      
+      
+      
+  logits = forward_pass(tf_train_dataset, dropout_rate)
+  loss = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+  loss += 0.5 * beta * (tf.nn.l2_loss(weights1) + tf.nn.l2_loss(weights2)+ tf.nn.l2_loss(weights3))
+  
+  # Optimizer.
+  #optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+  
+  # Predictions for the training, validation, and test data.
+  train_prediction = tf.nn.softmax(forward_pass(tf_train_dataset, 1))
+  valid_prediction = tf.nn.softmax(forward_pass(tf_valid_dataset, 1))
+  test_prediction = tf.nn.softmax(forward_pass(tf_test_dataset, 1))
+
+
+
+num_steps = 4001
+
+
+with tf.Session(graph=graph) as session:
+  tf.global_variables_initializer().run()
+  print("Initialized")
+  for step in range(num_steps):
+    # Pick an offset within the training data, which has been randomized.
+    # Note: we could use better randomization across epochs.
+    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+    # Generate a minibatch.
+    batch_data = train_dataset[offset:(offset + batch_size), :]
+    batch_labels = train_labels[offset:(offset + batch_size), :]
+    # Prepare a dictionary telling the session where to feed the minibatch.
+    # The key of the dictionary is the placeholder node of the graph to be fed,
+    # and the value is the numpy array to feed to it.
+    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+    _, l, predictions = session.run(
+      [optimizer, loss, train_prediction], feed_dict=feed_dict)
+    if (step % 500 == 0):
+      print("Minibatch loss at step %d: %f" % (step, l))
+      print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+      print("Validation accuracy: %.1f%%" % accuracy(
+        valid_prediction.eval(), valid_labels))
+  print("Beta = %f; Test accuracy: %.1f%%" % (beta, accuracy(test_prediction.eval(), test_labels)))
+  
+  """
+  Initialized
+Minibatch loss at step 0: 2.762476
+Minibatch accuracy: 7.8%
+Validation accuracy: 40.4%
+Minibatch loss at step 500: 0.698809
+Minibatch accuracy: 89.8%
+Validation accuracy: 85.6%
+Minibatch loss at step 1000: 0.777211
+Minibatch accuracy: 85.9%
+Validation accuracy: 86.5%
+Minibatch loss at step 1500: 0.541658
+Minibatch accuracy: 91.4%
+Validation accuracy: 87.8%
+Minibatch loss at step 2000: 0.526354
+Minibatch accuracy: 93.0%
+Validation accuracy: 87.9%
+Minibatch loss at step 2500: 0.534355
+Minibatch accuracy: 89.1%
+Validation accuracy: 88.3%
+Minibatch loss at step 3000: 0.578659
+Minibatch accuracy: 87.5%
+Validation accuracy: 88.4%
+Minibatch loss at step 3500: 0.561545
+Minibatch accuracy: 89.1%
+Validation accuracy: 88.7%
+Minibatch loss at step 4000: 0.457274
+Minibatch accuracy: 91.4%
+Validation accuracy: 89.1%
+Beta = 0.001000; Test accuracy: 95.0%
+
+Conclusion:  xavier_initializer is very import for ReLU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  """
